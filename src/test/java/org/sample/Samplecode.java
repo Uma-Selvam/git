f33fc9463d3b9ae6fa97eb3898da5f989348d14d
uma <umasit77@gmail.com>
2023-10-05 15:04:10 +0530

    git

---
+++ src/test/java/org/sample/Samplecode.java
@@ -0,0 +1,19 @@
+package org.sample;
+
+public class Samplecode {
+
+	public static void main(String[] args) {
+		String s="welcome to our college";
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+		if(rev.equals(s));
+		System.out.println(rev);
+							
+		}
+		
+		
+
+	}
+
+}
